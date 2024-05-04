3f407a59a2fb7703eda418f5ce8137d96ec40b59
vikashkumar82 <avikashkumar82@gmail.com>
2024-05-04 15:24:00 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult ResetPassword()
+        {
+            return View("ResetPwd");
+        }
+
+        public ViewResult ForgotPassword()
+        {
+            return View("ForgotPwd");
+        }
+
+        public ViewResult Kumar()
+        {
+            return View("~/Views/Vikash/Kumar.cshtml");
+        }
+    }
+}
